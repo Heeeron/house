a579ab46b8f2aaf7c77bc3c4e83831a484927067
anhailong <1130973200@qq.com>
2019-05-14 15:56:51 +0800

    two

--- src/main/java/com/learn/aop/SysLogAspect.java
+++ src/main/java/com/learn/aop/SysLogAspect.java
@@ -29,7 +29,7 @@ import java.util.Date;
 public class SysLogAspect {
 	@Autowired
 	private SysLogService sysLogService;
-	
+	//dsjafkldsajflsd
 	@Pointcut("@annotation(com.learn.annotation.SysLog)")
 	public void logPointCut() { 
 		
